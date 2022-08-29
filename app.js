b4706c18b85ef1a5adeedad6efa11c4e8820f3df
Coel Allen <coelallen@gmail.com>
2022-08-29 13:49:02 -0600

    start

---
+++ app.js
@@ -0,0 +1,30 @@
+let scoreOne = 0
+let scoreTwo = 0
+
+
+function addScoreOne() {
+  scoreOne++;
+  console.log(scoreOne);
+  drawScoreOne();
+
+}
+function addScoreTwo() {
+  scoreTwo++;
+  console.log(scoreTwo);
+  drawScoreTwo();
+
+}
+
+function drawScoreOne() {
+  let scoreOneElem = document.getElementById("scoreOne")
+
+}
+
+function drawScoreTwo() {
+  let scoreTwoElem = document.getElementById("scoreTwo")
+
+}
+
+function resetScores() {
+
+}
